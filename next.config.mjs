80309fe46d13b49852a8b48862b96d22bca85aaa
Rafay <rafayghafoor@pm.me>
2025-10-30 15:20:49 +0500

    feat: bootstrapping website

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  output: 'export', // Required for GitHub Pages (static HTML export)
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable Next image optimization for static export
   },
-}
+  assetPrefix: isProd ? '/rafa-store/' : '',
+  basePath: isProd ? '/rafa-store' : '',
+};
 
-export default nextConfig
+export default nextConfig;
